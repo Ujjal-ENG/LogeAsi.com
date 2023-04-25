9cba5dddb69d0b9888aa0c7bb929cb48d2d078ca
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-25 18:11:09 +0600

    successfully run the server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,24 @@
-// dependencies
-const http = require('http');
-// app object - module scaffolding
-const app = {};
-// configuration
-app.config = {};
+/* eslint-disable import/no-extraneous-dependencies */
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+
+// config dotenv file
+dotenv.config();
+
+// create the app
+const app = express();
+
+// setup the middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan());
+
+// PORT
+const { PORT } = process.env;
+
+// app listen
+app.listen(PORT, () => {
+    console.log(`server is listen on port ${PORT}`);
+});
