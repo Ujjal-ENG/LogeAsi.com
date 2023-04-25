d35336e13fe602201087ad37c6663db3bdd4cab4
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 01:07:43 +0600

    done the desing of the footer page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+/* eslint-disable react/jsx-indent */
+import { Outlet } from 'react-router-dom';
+import Footer from './components/layouts/Footer';
+import Navbar from './components/layouts/Navbar';
+
 function App() {
-    return <></>;
+    return (
+        <div>
+            <Navbar />
+            <main className="min-h-[90vh]">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+
+function Footer() {
+    return (
+        <div className="text-2xl font-bold text-center bg-black text-white py-2">
+            All Rights Reserved || <span className="text-red-600">©️</span> <span className="text-orange-500">Ujjal kumar Roy</span>
+        </div>
+    );
+}
+
+export default Footer;
