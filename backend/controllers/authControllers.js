f9851405bf6c7f21430482f775d23067fe7d47c0
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-25 20:28:17 +0600

    fixed some eslint error globally

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,8 +1,17 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable import/prefer-default-export */
 // registerUser
 export const registerUser = async (req, res) => {
     try {
-        res.send('Koppa mama');
+        const { name, email, password, phone, address } = req.body;
+
+        // validation
+        if (!name || !email || !password || !phone || !address) {
+            return res.status(404).json({
+                message: 'Please provided all fields value!!',
+                success: false,
+            });
+        }
     } catch (error) {
         res.status(404).json({
             message: 'Bad Request from Register',
--- backend/helpers/authHelper.js
+++ backend/helpers/authHelper.js
@@ -1,19 +1,14 @@
-/* eslint-disable consistent-return */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable max-len */
 /* eslint-disable prettier/prettier */
-/* eslint-disable no-shadow */
-/* eslint-disable import/prefer-default-export */
 import bcrypt from 'bcrypt';
 
 export const hashPassword = async (password) => {
     try {
         const saltRounds = 10;
-        const hashPassword = await bcrypt.hash(password, saltRounds);
-        return hashPassword;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        return hashedPassword;
     } catch (error) {
         console.log(error);
     }
 };
 
-export const comparePassword = async (password, hashPassword) => bcrypt.compare(password, hashPassword);
+export const comparePassword = async (password, hashedPassword) => bcrypt.compare(password, hashedPassword);
