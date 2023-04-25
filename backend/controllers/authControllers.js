6ab02ef71664f0fb7208255f333a6f5ed2ded83e
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-25 20:40:22 +0600

    successfully registered user

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,10 @@
+/* eslint-disable import/extensions */
 /* eslint-disable object-curly-newline */
 /* eslint-disable import/prefer-default-export */
+
+import { hashPassword } from '../helpers/authHelper.js';
+import userModel from '../models/userModel.js';
+
 // registerUser
 export const registerUser = async (req, res) => {
     try {
@@ -12,6 +17,28 @@ export const registerUser = async (req, res) => {
                 success: false,
             });
         }
+
+        // existing user
+        const isExistUser = await userModel.findOne({ email });
+
+        if (isExistUser) {
+            return res.status(200).json({
+                message: 'Already Registered!!!',
+                success: true,
+            });
+        }
+
+        // registerUser
+        const hashedPassowrd = await hashPassword(password);
+
+        // save user
+        const user = await userModel.create({ ...req.body, password: hashedPassowrd });
+
+        res.status(201).json({
+            message: 'User is Successfully registered',
+            success: true,
+            user,
+        });
     } catch (error) {
         res.status(404).json({
             message: 'Bad Request from Register',
