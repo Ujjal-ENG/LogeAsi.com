a1e7be3b91399d9de993138399d5dc17f84bc650
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-25 22:26:21 +0600

    Login functionality successfully done

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable import/extensions */
 /* eslint-disable object-curly-newline */
 /* eslint-disable import/prefer-default-export */
 
-import { hashPassword } from '../helpers/authHelper.js';
+import jwt from 'jsonwebtoken';
+import { comparePassword, hashPassword } from '../helpers/authHelper.js';
 import userModel from '../models/userModel.js';
 
 // registerUser
@@ -42,6 +44,58 @@ export const registerUser = async (req, res) => {
     } catch (error) {
         res.status(404).json({
             message: 'Bad Request from Register',
+            success: false,
+            error,
+        });
+    }
+};
+
+// login User
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // validation
+        if (!email || !password) {
+            return res.status(400).json({
+                message: 'Please provide Email and Password!!',
+                success: false,
+            });
+        }
+
+        // check email exist or not
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'Please Register first!!',
+                success: false,
+            });
+        }
+
+        // match password
+        const matchPassword = await comparePassword(password, user.password);
+        if (!matchPassword) {
+            return res.status(400).json({
+                message: 'Invalid email or password!!',
+                success: false,
+            });
+        }
+
+        // create token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '7d',
+        });
+
+        res.status(200).json({
+            message: 'Login Successfully!!',
+            success: true,
+            user,
+            token,
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: 'Bad Request from LoginUser',
             success: false,
             error,
         });
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,13 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import { registerUser } from '../controllers/authControllers.js';
+import { loginUser, registerUser } from '../controllers/authControllers.js';
 
 const router = express.Router();
 
 // register router
 router.post('/register', registerUser);
+
+// login router
+router.post('/login', loginUser);
 
 export default router;
