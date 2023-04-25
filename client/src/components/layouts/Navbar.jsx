dd8d7d213dd32b6f7d8e741c04affcaca3f97fc7
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 01:19:38 +0600

    navbar worked properly

--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -5,10 +5,18 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className="flex justify-center items-center gap-8 py-3 shadow-lg text-2xl font-bold">
-            <NavLink className={({ isActive }) => (isActive ? 'active' : 'default:')}>Home</NavLink>
-            <NavLink className={({ isActive }) => (isActive ? 'active' : 'default:')}>About</NavLink>
-            <NavLink className={({ isActive }) => (isActive ? 'active' : 'default:')}>Contact</NavLink>
-            <NavLink className={({ isActive }) => (isActive ? 'active' : 'default:')}>Policy</NavLink>
+            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'default:')}>
+                Home
+            </NavLink>
+            <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'default:')}>
+                About
+            </NavLink>
+            <NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : 'default:')}>
+                Contact
+            </NavLink>
+            <NavLink to="/policy" className={({ isActive }) => (isActive ? 'active' : 'default:')}>
+                Policy
+            </NavLink>
         </nav>
     );
 }
--- client/src/components/routes/App.Routes.jsx
+++ client/src/components/routes/App.Routes.jsx
@@ -2,8 +2,11 @@
 /* eslint-disable comma-dangle */
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../../App';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
 import ErrorPage from '../pages/ErrorPage';
 import Home from '../pages/Home';
+import Policy from '../pages/Policy';
 
 export const router = createBrowserRouter([
     {
@@ -14,6 +17,18 @@ export const router = createBrowserRouter([
             {
                 path: 'home',
                 element: <Home />
+            },
+            {
+                path: 'about',
+                element: <About />
+            },
+            {
+                path: 'contact',
+                element: <Contact />
+            },
+            {
+                path: 'policy',
+                element: <Policy />
             }
         ]
     }
