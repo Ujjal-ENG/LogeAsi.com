632b4aef1d441b972ac0cc48e1531f49dd1060cd
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 17:59:16 +0600

    logged in successfully

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable react/jsx-indent-props */
@@ -7,9 +8,40 @@
 /* eslint-disable max-len */
 /* eslint-disable react/jsx-indent */
 /* eslint-disable react/jsx-no-undef */
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.id]: e.target.value
+        });
+    };
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('http://localhost:8080/api/v1/auth/login', user);
+
+            if (res.data && res.data.success) {
+                toast.success('Successfully LoggedIn!!');
+                navigate('/');
+            } else {
+                console.log(res.data.message);
+                toast.error(res.data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -18,28 +50,28 @@ function Login() {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input id="email" type="email" placeholder="email" value={user.email} required onChange={handleChange} className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" placeholder="password" id="password" required className="input input-bordered" value={user.password} onChange={handleChange} />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">
                                     Forgot password?
                                 </a>
                             </label>
                         </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                        <div type="submit" className="form-control mt-6">
+                            <button className="btn btn-success">Login</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
