ea2de4fd4442118b259ed091d923cde23ee07ace
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 16:03:42 +0600

    done the about page

--- client/src/components/pages/FooterPage/About.jsx
+++ client/src/components/pages/FooterPage/About.jsx
@@ -1,7 +1,25 @@
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react';
 
 function About() {
-    return <div>About</div>;
+    return (
+        <div className="flex justify-between items-center sm:flex-row flex-col px-8 py-10">
+            <iframe src="https://embed.lottiefiles.com/animation/84565" className="p-24" />
+            <p className="text-gray-600 font-bold text-justify max-w-4xl">
+                Welcome to LogeAsi.com, your destination for affordable and sustainable fashion. Our mission is to provide stylish and eco-friendly clothing that helps our customers feel good about
+                themselves and the planet. Our story began in 2015 when our founder, Lily, realized the negative impact that fast fashion was having on the environment. She decided to take matters
+                into her own hands and create a fashion brand that prioritizes sustainability and ethical production practices. At LogeAsi.com, we believe that fashion should not come at the expense
+                of the planet or its people. That's why we use only high-quality, organic, and recycled materials in our clothing, and we partner with ethical manufacturers who prioritize fair labor
+                practices. Our team is made up of fashion enthusiasts who are passionate about sustainable fashion and its potential to make a positive impact on the world. We are committed to
+                providing excellent customer service and helping our customers find clothing that they love and feel good about wearing. We take pride in our commitment to sustainability and
+                transparency. We are constantly working to improve our processes and reduce our environmental footprint. We are also proud to offer a range of sizing options to ensure that all of our
+                customers can find clothing that fits and flatters their unique bodies. Thank you for choosing LogeAsi.com for your sustainable fashion needs. We are excited to be part of your journey
+                towards a more conscious and eco-friendly lifestyle.
+            </p>
+        </div>
+    );
 }
 
 export default About;
