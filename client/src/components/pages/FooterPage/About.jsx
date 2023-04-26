eb0f4cf633d30cb7f42da9f12ec9c108dbe4328f
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 16:17:59 +0600

    done the footers pages

--- client/src/components/pages/FooterPage/About.jsx
+++ client/src/components/pages/FooterPage/About.jsx
@@ -1,24 +1,84 @@
-/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable max-len */
 /* eslint-disable react/jsx-indent */
-/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable react/jsx-no-undef */
 import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 function About() {
     return (
-        <div className="flex justify-between items-center sm:flex-row flex-col px-8 py-10">
-            <iframe src="https://embed.lottiefiles.com/animation/84565" className="p-24" />
-            <p className="text-gray-600 font-bold text-justify max-w-4xl">
-                Welcome to LogeAsi.com, your destination for affordable and sustainable fashion. Our mission is to provide stylish and eco-friendly clothing that helps our customers feel good about
-                themselves and the planet. Our story began in 2015 when our founder, Lily, realized the negative impact that fast fashion was having on the environment. She decided to take matters
-                into her own hands and create a fashion brand that prioritizes sustainability and ethical production practices. At LogeAsi.com, we believe that fashion should not come at the expense
-                of the planet or its people. That's why we use only high-quality, organic, and recycled materials in our clothing, and we partner with ethical manufacturers who prioritize fair labor
-                practices. Our team is made up of fashion enthusiasts who are passionate about sustainable fashion and its potential to make a positive impact on the world. We are committed to
-                providing excellent customer service and helping our customers find clothing that they love and feel good about wearing. We take pride in our commitment to sustainability and
-                transparency. We are constantly working to improve our processes and reduce our environmental footprint. We are also proud to offer a range of sizing options to ensure that all of our
-                customers can find clothing that fits and flatters their unique bodies. Thank you for choosing LogeAsi.com for your sustainable fashion needs. We are excited to be part of your journey
-                towards a more conscious and eco-friendly lifestyle.
-            </p>
+        <div className="bg-white flex justify-center items-center w-full">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+                    <div>
+                        <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">Who We Are</h2>
+                        <p className="mt-3 text-lg text-gray-500">
+                            LogeAsi.com is a leading online retailer of high-quality products for customers in the United States and beyond. Our mission is to provide a convenient and reliable
+                            shopping experience, with a focus on exceptional customer service and affordable prices.
+                        </p>
+                        <div className="mt-8 sm:flex">
+                            <div className="flow-root">
+                                <a href="#" className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                                    <FaTwitter className="flex-shrink-0 h-6 w-6 text-gray-400" />
+                                    <span className="ml-3">Twitter</span>
+                                </a>
+                            </div>
+                            <div className="flow-root mt-6 sm:mt-0 sm:ml-6">
+                                <a href="#" className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                                    <FaFacebook className="flex-shrink-0 h-6 w-6 text-gray-400" />
+                                    <span className="ml-3">Facebook</span>
+                                </a>
+                            </div>
+                            <div className="flow-root mt-6 sm:mt-0 sm:ml-6">
+                                <a href="#" className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                                    <FaInstagram className="flex-shrink-0 h-6 w-6 text-gray-400" />
+                                    <span className="ml-3">Instagram</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+                        <div className="h-full flex flex-col justify-between py-6 px-4 bg-gray-50 rounded-xl">
+                            <div className="pb-5">
+                                <h3 className="text-2xl font-extrabold text-gray-900">Our Values</h3>
+                                <p className="mt-3 text-lg text-gray-500">
+                                    At LogeAsi.com, we believe in providing the highest-quality products and services to our customers. We are committed to maintaining the highest standards of ethics
+                                    and integrity in all our business practices. Our values include:
+                                </p>
+                                <ul className="mt-5 text-lg text-gray-500 list-disc list-inside">
+                                    <li>Integrity and honesty</li>
+                                    <li>Commitment to excellence</li>
+                                    <li>Respect for customers and employees</li>
+                                    <li>Innovation and creativity</li>
+                                </ul>
+                            </div>
+                            <div className="text-lg text-gray-500">
+                                <p>
+                                    LogeAsi.com is a subsidiary of XYZ Inc., a global leader in e-commerce and technology solutions. With over 20 years of experience in the industry, we have built a
+                                    reputation for excellence and innovation, and we continue to strive towards new heights in customer service and satisfaction.
+                                </p>
+                                <p className="mt-4">
+                                    Thank you for choosing LogeAsi.com as your preferred shopping destination. We look forward to serving you and exceeding your expectations every time you shop with
+                                    us.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="mx-auto max-w-xs absolute inset-0 rounded-full overflow-hidden pointer-events-none">
+                            <svg className="absolute bottom-0 right-0 mb-8 transform translate-x-1/2" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                                <defs>
+                                    <pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                                        <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                                    </pattern>
+                                </defs>
+                                <rect width="404" height="384" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+                            </svg>
+                            <div className="flex justify-center -mt-12">
+                                <img className="rounded-full bg-white p-2" src="https://dummyimage.com/150x150/4A5568/ffffff" alt="" width="150" height="150" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/components/pages/FooterPage/Policy.jsx
+++ client/src/components/pages/FooterPage/Policy.jsx
@@ -1,7 +1,90 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable max-len */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-no-undef */
 import React from 'react';
+import { FaFacebook, FaInstagram, FaPhoneAlt, FaRegEnvelope, FaTwitter } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
 
 function Policy() {
-    return <div>Policy</div>;
+    return (
+        <div className="bg-gray-100">
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-lg mx-auto">
+                    <h2 className="text-3xl font-bold text-gray-900">Privacy Policy</h2>
+                    <p className="mt-4 text-gray-500">
+                        At LogeAsi.com, we take your privacy seriously. This Privacy Policy describes how we collect, use, and share information about you when you use our website or contact us.
+                    </p>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900">Information We Collect</h3>
+                    <p className="mt-4 text-gray-500">
+                        We collect information about you when you visit our website, sign up for our newsletter, or contact us directly. This information may include your name, email address, phone
+                        number, and other details you provide to us.
+                    </p>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900">How We Use Information</h3>
+                    <p className="mt-4 text-gray-500">
+                        We use the information we collect to improve our website and services, send you newsletters and other updates, respond to your inquiries, and communicate with you about our
+                        business.
+                    </p>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900">Information We Share</h3>
+                    <p className="mt-4 text-gray-500">We do not share your information with third parties except as necessary to provide our services or as required by law.</p>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900">Cookies and Tracking</h3>
+                    <p className="mt-4 text-gray-500">
+                        We use cookies and other tracking technologies to improve your experience on our website, analyze our traffic, and personalize our content. You can control cookies through your
+                        browser settings.
+                    </p>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900">Contact Us</h3>
+                    <p className="mt-4 text-gray-500">If you have any questions or concerns about our Privacy Policy, please contact us at:</p>
+                    <div className="mt-4 flex items-center">
+                        <div className="flex-shrink-0">
+                            <HiOutlineMail className="h-6 w-6 text-gray-400" />
+                        </div>
+                        <div className="ml-3">
+                            <p className="text-sm font-medium text-gray-500">
+                                <a href="mailto:contact@logeasi.com">contact@logeasi.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mt-4 flex items-center">
+                        <div className="flex-shrink-0">
+                            <FaPhoneAlt className="h-6 w-6 text-gray-400" />
+                        </div>
+                        <div className="ml-3">
+                            <p className="text-sm font-medium text-gray-500">
+                                <a href="tel:+1-555-555-5555">+1 (555555-5555)</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mt-4 flex items-center">
+                        <div className="flex-shrink-0">
+                            <FaRegEnvelope className="h-6 w-6 text-gray-400" />
+                        </div>
+                        <div className="ml-3">
+                            <p className="text-sm font-medium text-gray-500">
+                                123 Main St, Suite 500
+                                <br />
+                                Anytown, USA 12345
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mt-4 flex">
+                        <a href="#" className="text-gray-400 hover:text-gray-500">
+                            <span className="sr-only">Twitter</span>
+                            <FaTwitter className="h-6 w-6" />
+                        </a>
+                        <a href="#" className="ml-3 text-gray-400 hover:text-gray-500">
+                            <span className="sr-only">Facebook</span>
+                            <FaFacebook className="h-6 w-6" />
+                        </a>
+                        <a href="#" className="ml-3 text-gray-400 hover:text-gray-500">
+                            <span className="sr-only">Instagram</span>
+                            <FaInstagram className="h-6 w-6" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Policy;
