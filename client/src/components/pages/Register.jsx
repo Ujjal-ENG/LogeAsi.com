f0346d6e7f2b394a259702c3e26fc43e707e9983
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 16:41:08 +0600

    fixed some issues

--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -125,7 +125,7 @@ function Register() {
                                     autoComplete="role"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 pl-10 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
-                                    <option value="" disabled selected>
+                                    <option disabled defaultValue="">
                                         Select Role
                                     </option>
                                     <option value="customer">Customer</option>
