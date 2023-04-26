6838f6a0a17a61a267bc50e9f644b5035ca66589
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-27 03:26:22 +0600

    admin menu shown

--- client/src/components/layouts/AdminMenu.jsx
+++ client/src/components/layouts/AdminMenu.jsx
@@ -1,7 +1,24 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function AdminMenu() {
-    return <div>AdminMenu</div>;
+    return (
+        <div>
+            <h1 className="text-3xl font-bold py-4">Admin Panel</h1>
+            <div className="btn-group btn-group-vertical w-full">
+                <NavLink to="/dashboard/admin/create-category" className="btn  btn-outline">
+                    Create Category
+                </NavLink>
+                <NavLink to="/dashboard/admin/create-product" className="btn  btn-outline ">
+                    Create Product
+                </NavLink>
+                <NavLink to="/dashboard/admin/users" className="btn  btn-outline ">
+                    Users
+                </NavLink>
+            </div>
+        </div>
+    );
 }
 
 export default AdminMenu;
--- client/src/components/pages/Admin/AdminDashboard.jsx
+++ client/src/components/pages/Admin/AdminDashboard.jsx
@@ -1,7 +1,16 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import AdminMenu from '../../layouts/AdminMenu';
 
 function AdminDashboard() {
-    return <div>AdminDashboard</div>;
+    return (
+        <div className="grid grid-cols-12 justify-items-center  mx-auto">
+            <div className="col-span-3 w-full px-8">
+                <AdminMenu />
+            </div>
+            <div className="bg-black col-span-9">hell</div>
+        </div>
+    );
 }
 
 export default AdminDashboard;
