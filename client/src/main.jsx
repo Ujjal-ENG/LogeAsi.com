1ca6bbb9fa9a2772cc489414b52b53efe043287b
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 20:22:32 +0600

    fixed some silly bugs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './components/routes/App.Routes';
 import AuthProvider from './context/AuthProvider';
 import './index.css';
+import { router } from './routes/App.Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
--- client/src/routes/App.Routes.jsx
+++ client/src/routes/App.Routes.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable comma-dangle */
 import { createBrowserRouter } from 'react-router-dom';
-import App from '../../App';
-import Category from '../pages/Category';
-import ErrorPage from '../pages/ErrorPage';
-import About from '../pages/FooterPage/About';
-import Contact from '../pages/FooterPage/Contact';
-import Policy from '../pages/FooterPage/Policy';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Dashbroad from '../pages/user/Dashbroad';
+import App from '../App';
+import Category from '../components/pages/Category';
+import ErrorPage from '../components/pages/ErrorPage';
+import About from '../components/pages/FooterPage/About';
+import Contact from '../components/pages/FooterPage/Contact';
+import Policy from '../components/pages/FooterPage/Policy';
+import Home from '../components/pages/Home';
+import Login from '../components/pages/Login';
+import Register from '../components/pages/Register';
+import Dashbroad from '../components/pages/user/Dashbroad';
 
 export const router = createBrowserRouter([
     {
