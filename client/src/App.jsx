4db8883420c9e7406a700e815773934ac5b961b7
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 16:27:51 +0600

    seo not doen yet we are working on end of the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/jsx-indent */
+import { Helmet } from 'react-helmet';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/layouts/Footer';
 import Navbar from './components/layouts/Navbar';
 
-function App() {
+function App({ title, description, keywords }) {
     return (
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="description" content={description || 'Daily uses products'} />
+                <meta name="keywords" content={keywords || 'e-commerce,products,product'} />
+                <meta name="author" content="Ujjal Kumar Roy" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>{title || 'LogeAsi'}</title>
+            </Helmet>
             <Navbar />
             <main className="min-h-[86.2vh]">
                 <Outlet />
