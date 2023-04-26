15409508993580633c0272538100389704d84bab
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-27 02:04:46 +0600

    register now value selection worked better from prevoouius one like a slily bugs fixed

--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -196,11 +196,11 @@ function Register() {
                                     onChange={handleChange}
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 pl-10 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
-                                    <option disabled defaultValue="">
+                                    <option disabled defaultValue={0}>
                                         Select Role
                                     </option>
-                                    <option value="0">Customer</option>
-                                    <option value="1">Vendor</option>
+                                    <option value={0}>Customer</option>
+                                    <option value={1}>Vendor</option>
                                 </select>
                             </div>
                         </div>
