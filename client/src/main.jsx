be45233a68b926d12977427567a6865859c66fee
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-26 16:52:36 +0600

    added axios and react hot toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,15 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './components/routes/App.Routes';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        <Toaster />
+        <RouterProvider router={router} />
+    </>
+);
