1c1fffe0141c1761d55358994e59e1ef2c5965e1
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-27 21:33:51 +0600

    create category worked properly

--- backend/controllers/categoryControllers.js
+++ backend/controllers/categoryControllers.js
@@ -1,7 +1,38 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/prefer-default-export */
 /* eslint-disable no-empty */
 /* eslint-disable linebreak-style */
+
+import slugify from 'slugify';
+import categoryModel from '../models/categoryModel.js';
+
 export const createCategory = async (req, res) => {
     try {
+        const { name } = req.body;
+
+        if (!name) {
+            return res.status(401).json({
+                message: 'Name is Required',
+                success: false,
+            });
+        }
+
+        const exisitingCategory = await categoryModel.findOne({ name });
+
+        if (exisitingCategory) {
+            return res.status(200).json({
+                message: 'Category Already Exists',
+                success: false,
+            });
+        }
+
+        const category = await categoryModel.create({ name, slug: slugify(name) });
+
+        res.status(201).json({
+            message: 'Category is Created Successfully!!',
+            success: true,
+            category,
+        });
     } catch (error) {
         res.status(500).json({
             message: 'Error in Category',
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import { createCategory } from '../controllers/categoryControllers.js';
+import { createCategory,updateCategory } from '../controllers/categoryControllers.js';
 import { isAdmin, requireSignIn } from '../middlewares/authMiddlewares.js';
 
 const router = express.Router();
@@ -8,6 +8,9 @@ const router = express.Router();
 // routes
 
 // create category
-router.post('create-category', requireSignIn, isAdmin, createCategory);
+router.post('/create-category', requireSignIn, isAdmin, createCategory);
+
+// update category
+router.patch("/update-category",requireSignIn,isAdmin,updateCategory)
 
 export default router;
