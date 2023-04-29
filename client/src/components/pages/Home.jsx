af552e5bbf1e79d375070abe76e2f6d2b1753eea
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-29 19:19:38 +0600

    now design the home page

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,11 +1,17 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 
 function Home() {
-    const a = 'c';
     return (
-        <>
-            <h1>hekkko</h1>
-        </>
+        <div className="grid grid-cols-12 gap-4 px-5 pt-5">
+            <div className="col-span-3">
+                <h6 className="text-xl font-bold">Filter By Category</h6>
+            </div>
+
+            <div className="col-span-9 w-full ">
+                <h1 className=" font-bold text-3xl text-center w-full">All Products</h1>
+            </div>
+        </div>
     );
 }
 
