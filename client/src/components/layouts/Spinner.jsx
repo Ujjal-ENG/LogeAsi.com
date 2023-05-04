b9c4778fda01cf05050c1edf88b51dd79e50a147
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-05 02:23:52 +0600

    some problem are occured when spinner function i tryto delete some portion

--- client/src/components/layouts/Spinner.jsx
+++ client/src/components/layouts/Spinner.jsx
@@ -5,20 +5,27 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable react/jsx-indent */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 function Spinner() {
+    const [count, setCount] = useState(5);
     const location = useLocation();
     const navigate = useNavigate();
 
     useEffect(() => {
-        navigate('/login', {
-            state: location.pathname
-        });
-    }, [location.pathname, navigate]);
+        const interval = setInterval(() => {
+            setCount((ps) => --ps);
+        }, 1000);
+        count === 0 &&
+            navigate('/login', {
+                state: location.pathname
+            });
+        return () => clearInterval(interval);
+    }, [count, navigate, location]);
     return (
         <div className="flex flex-col space-y-8 h-screen justify-center items-center">
+            <h1 className="text-center text-2xl font-bold">Redirecting to you in {count} second</h1>
             <progress className="progress w-56" />
         </div>
     );
